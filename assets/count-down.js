fc6f7cb609c93bc5ca48ead6a4b9e9e41aad2f77
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-03 09:18:28 +0300

    add for loop

---
+++ assets/count-down.js
@@ -0,0 +1,26 @@
+
+const output = document.querySelector('.output')
+output.innerHTML = '';
+
+for (i = 10; i > 0; i--){
+    const para = document.createElement('p');
+    output.appendChild(para);
+      
+ if (i === 10){
+        para.textContent = "count down 10" ;
+    
+ }
+ else if(i === 0){
+ 
+        para.textContent ="Blast off!" ;
+    
+ }
+ else {
+
+        para.textContent = [i] ;
+     
+ }
+
+}
+
+
